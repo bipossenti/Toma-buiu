c5acf4461a166976e5f6c907563517336c81d90c
bianca possenti <107114103+bipossenti@users.noreply.github.com>
2024-09-05 22:33:18 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,8 +1,9 @@
 const nomes = ["Você é..."];
 
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
+export function aleatorio(lista) {
+    if (lista.length === 0) return ""; // Prevenir erro se a lista estiver vazia
+    const posicao = Math.floor(Math.random() * lista.length);
     return lista[posicao];
 }
 
-export const nome = aleatorio(nomes)
+export const nome = aleatorio(nomes);
