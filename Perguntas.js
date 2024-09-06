c78a7c6b7abca96a38242965be208d4b179e70dd
bianca possenti <107114103+bipossenti@users.noreply.github.com>
2024-09-05 21:45:52 -0300

    Create Perguntas.js

---
+++ Perguntas.js
@@ -0,0 +1,153 @@
+export const perguntas = [
+    {
+        enunciado: "Como você reage em uma situação de conflito?",
+        alternativas: [
+            {
+                texto: "Tento resolver pacificamente e encontrar um meio-termo!",
+                afirmacao: [
+                    "Você seria **Obi-Wan Kenobi**!"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Uso minha força e autoridade para impor minha vontade!",
+                afirmacao: [
+                    "Você seria **Darth Vader**!"
+           
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a sua abordagem em relação a regras e autoridade?",
+        alternativas: [
+            {
+                texto: "Acredito que as regras existem para serem seguidas, mas posso fazer exceções para o bem maior.",
+                afirmacao: [
+                    "Sua abordagem é centrada na compaixão,"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "As regras são feitas para serem quebradas se isso me ajudar a alcançar meus objetivos.",
+                afirmacao: [
+                    "Sua ambição, desejo de controle e disposição"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quando se trata de lealdade, você é:",
+        alternativas: [
+            {
+                texto: "Leal a amigos e aliados, mesmo que isso signifique sacrificar meus próprios interesses.",
+                afirmacao: [
+                    "aprende com suas experiências, "
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Leal apenas a quem pode me ajudar a alcançar meu poder e ambições.",
+                afirmacao: [
+                    "para desafiar as normas fazem de"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você lida com o passado?",
+        alternativas: [
+            {
+                texto: "Aprendo com meus erros e busco redenção.  ",
+                afirmacao: [
+                    "sempre buscando fazer o bem."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Minha história é um símbolo de poder e não me arrependo das minhas escolhas.",
+                afirmacao: [
+                    "você uma força poderosa e imponente."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você tivesse que escolher um lema, qual seria?",
+        alternativas: [
+            {
+                texto: "A força está com aqueles que buscam a paz.",
+                afirmacao: [
+                    "um cavaleiro Jedi,"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "O poder é a verdadeira essência do controle.",
+                afirmacao: [
+                    "Você acredita que o poder é essencial para alcançar seus objetivos,"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você se deparasse com um inimigo, qual seria sua abordagem??",
+        alternativas: [
+            {
+                texto: "Tentaria dialogar e encontrar um entendimento.",
+                afirmacao: [
+                    "é um dos personagens mais respeitados da saga Star Wars. "
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Enfrentaria diretamente, mostrando quem é o mais forte.",
+                afirmacao: [
+                    "mesmo que isso signifique tomar decisões difíceis."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você se sente em relação ao passado?",
+        alternativas: [
+            {
+                texto: "Vejo-o como uma parte importante do meu crescimento e aprendizado.",
+                afirmacao: [
+                    "Ele foi o mentor de Anakin Skywalker."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "É algo que tento esquecer ou usar para me tornar mais forte.",
+                afirmacao: [
+                    "Originalmente um Jedi talentoso, ele sucumbe ao lado sombrio da Força."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu lema na vida?",
+        alternativas: [
+            {
+                texto: "A verdadeira força vem da compaixão.",
+                afirmacao: [
+                    "Obi-Wan é um duelista habilidoso, conhecido por seu estilo de luta defensivo e pela famosa frase "Eu sou o mestre agora"."
+                ],
+            },
+            {
+                texto: "Somente os fortes sobrevivem.",
+                afirmacao: [
+                    "temido por muitos, não apenas por suas habilidades de combate, mas pela sua presença imponente."
+                ],
+            },
+        ]
+    }
+];
